adcbb9af4c42c1b0a1e0b1f9b6a7d40ea13a2d55
Javier Ruiz <javier.ruiz.cadena@gmail.com>
2022-03-08 18:26:40 -0600

    Create feature working Form to create employee was added
    Co-authored-by: Elías U. Rodríguez <EliasUR@users.noreply.github.com>

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,14 @@
 class EmployeesController < ApplicationController
     def index
-    @employees = Employee.all
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+    Employee.create name: params['name'], position: params["position"], email:["email"], private_number:["private_number"]
+    redirect_to action: :index
     end
 end
