1a97b2f48d28443b1ea0801b3d0f8dfe7ce9a526
Javier Ruiz <javier.ruiz.cadena@gmail.com>
2022-03-10 15:43:50 -0600

    Indetation and blank line was added

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,30 +1,34 @@
+# frozen_string_literal: true
+
 class EmployeesController < ApplicationController
-    def index
-        @employees = Employee.all
-    end
+  def index
+    @employees = Employee.all
+  end
 
-    def new
-        @employee = Employee.new
-    end
+  def new
+    @employee = Employee.new
+  end
 
-    def create
-        Employee.create name: params['name'], position: params["position"], email:["email"], private_number:["private_number"]
-        redirect_to action: :index
-    end
+  def create
+    Employee.create name: params['name'], position: params['position'], email: ['email'],
+                    private_number: ['private_number']
+    redirect_to action: :index
+  end
 
-    def edit
-        @employee = Employee.find(params['id'])
-    end
+  def edit
+    @employee = Employee.find(params['id'])
+  end
 
-    def update
-        employee = Employee.find(params['id'])
-        employee.update name: params['name'], position: params["position"], email:["email"], private_number:["private_number"]
-        redirect_to action: :index
-    end
+  def update
+    employee = Employee.find(params['id'])
+    employee.update name: params['name'], position: params['position'], email: ['email'],
+                    private_number: ['private_number']
+    redirect_to action: :index
+  end
 
-    def show
-        employee = Employee.find(params['id'])
-        employee.update position: "Desactive"
-        redirect_to action: :index
-    end
+  def show
+    employee = Employee.find(params['id'])
+    employee.update position: 'Desactive'
+    redirect_to action: :index
+  end
 end
