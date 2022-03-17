674ad97c4b734193080b7d28653fd15acffeb254
femm34 <femm15.mm@gmail.com>
2022-03-16 20:27:54 -0600

    modifying .scss files
    modify the application.sass.scss to import bootstrap, also modified the script build:css to compile sass

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_233900) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_14_210822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "branches", force: :cascade do |t|
     t.string "name"
@@ -26,8 +34,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_233900) do
     t.string "name"
     t.string "position"
     t.integer "private_number"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "records", force: :cascade do |t|
+    t.bigint "employee_id"
+    t.datetime "check_in"
+    t.datetime "check_out"
+    t.integer "hours"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_records_on_employee_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "records", "employees"
 end
