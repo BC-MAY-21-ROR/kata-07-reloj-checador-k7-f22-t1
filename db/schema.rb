dc9acd0f3903db2feab0f64e097fc5a09c76f5a9
Javier Ruiz <javier.ruiz.cadena@gmail.com>
2022-03-16 18:33:53 -0600

    Feuture to do check in/out working

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_03_14_210822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "email"
     t.string "name"
@@ -20,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_210822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -40,18 +40,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_210822) do
   create_table "employees", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.integer "role_id"
+    t.bigint "role_id"
+    t.string "status"
     t.integer "private_number"
     t.integer "status"
+    t.bigint "branch_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "records", force: :cascade do |t|
-    t.integer "employee_id"
+    t.bigint "employee_id"
     t.datetime "check_in"
     t.datetime "check_out"
-    t.integer "hours"
+    t.decimal "hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["employee_id"], name: "index_records_on_employee_id"
