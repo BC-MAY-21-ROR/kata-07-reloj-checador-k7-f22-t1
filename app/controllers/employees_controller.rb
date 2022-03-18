18d26114d7d2d9813b93391faa643c369347de89
EliasUR <eliasrodriguez2002@hotmail.com>
2022-03-17 21:00:27 -0300

    Complete Employees View
    Changed name of show method which made the deactivate fuction to  update_status method. Added a new route for update_status.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,7 +3,7 @@
 class EmployeesController < ApplicationController
   before_action :new_form, only: [:new, :edit]
   before_action :list_employees, only: [:new, :index, :edit]
-  before_action :find_employee, only: [:edit, :update, :show]
+  before_action :find_employee, only: [:edit, :update, :update_status]
   after_action :close_form, only: [:create, :update]
   def index
   end
@@ -14,7 +14,8 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    Employee.create name: params[:employee][:name], role_id: params['role'], 
+    name = params[:employee][:name].upcase
+    Employee.create name: name, role_id: params['role'], 
                     email: params[:employee][:email], branch_id: params['branch'], 
                     private_number: params[:employee][:private_number], status: true
     redirect_to action: :index
@@ -25,14 +26,16 @@ class EmployeesController < ApplicationController
   end
 
   def update
-    @employee.update(name: params[:employee][:name], role_id: params['role'], 
+    name = params[:employee][:name].upcase
+    @employee.update(name: name, role_id: params['role'], 
                     email: params[:employee][:email], branch_id: params['branch'], 
                     private_number: params[:employee][:private_number])
     redirect_to action: :index
   end
 
-  def show
-    @employee.update status: false
+  def update_status
+    status = !@employee.status
+    @employee.update status: status
     redirect_to action: :index
   end
 
@@ -44,7 +47,7 @@ class EmployeesController < ApplicationController
 
   def list_employees
     if params["name"]
-      employees = Employee.where("name LIKE ?", "%#{params["name"]}%")
+      employees = Employee.where("name LIKE ?", "%#{params["name"].upcase}%")
     else
       employees = Employee.all
     end
