86be45f0ae1d51af9fb8b9f56ba8cd222db62185
femm34 <femm15.mm@gmail.com>
2022-03-18 21:51:31 -0600

    main view of branches finished
    i implemented js to show a form and also hide a button, i changed some styles

--- app/javascript/controllers/branches_controller.js
+++ app/javascript/controllers/branches_controller.js
@@ -1,15 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="branches"
 export default class extends Controller {
-  static targets = [ "name" ]
+  static targets = ["form_branches", "button_new"];
+  // static targets = ["button_new_branch"]
 
   connect() {
-    console.log("Hello, Stimulus!", this.element)
+    console.log("Hello, Stimulus!", this.element);
   }
 
-  showAlert(){
-    this.nameTarget.classList.remove("d-none")
-    // console.log("Hello, Stimulus!", this.element)
+  hideButton() {
+    this.button_newTarget.classList.add("d-none");
+  }
+
+  showForm() {
+    this.form_branchesTarget.classList.remove("d-none");
+    this.hideButton();
   }
 }
