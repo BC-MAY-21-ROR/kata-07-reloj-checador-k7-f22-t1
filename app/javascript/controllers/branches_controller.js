6082e1615d918e5473e8a342f4334ea0b5e33004
femm34 <femm15.mm@gmail.com>
2022-03-22 14:17:44 -0600

    cleaning file
    i removed unused code

--- app/javascript/controllers/branches_controller.js
+++ app/javascript/controllers/branches_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="branches"
 export default class extends Controller {
   static targets = ["form_branches", "button_new"];
-  // static targets = ["button_new_branch"]
-
-  connect() {
-    console.log("Hello, Stimulus!", this.element);
-  }
 
   hideButton() {
     this.button_newTarget.classList.add("d-none");
