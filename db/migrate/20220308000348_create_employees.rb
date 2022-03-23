8dc8f514ddfa28da65ef9464799d7c55f847ee18
EliasUR <eliasrodriguez2002@hotmail.com>
2022-03-23 20:19:37 -0300

    Add change from master

--- db/migrate/20220308000348_create_employees.rb
+++ db/migrate/20220308000348_create_employees.rb
@@ -4,7 +4,7 @@ class CreateEmployees < ActiveRecord::Migration[7.0]
       t.string :email
       t.string :name
       t.belongs_to :role, foreign_key: true
-      t.string :status
+      t.boolean :status
       t.integer :private_number
       t.belongs_to :branch, foreign_key: true
       t.timestamps
