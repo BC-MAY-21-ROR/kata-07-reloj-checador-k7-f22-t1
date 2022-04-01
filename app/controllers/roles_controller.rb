b0313cc9eabe8512b117b39c0f61eb4ce3ce753c
Javi1499 <fruiz1@ucol.mx>
2022-03-31 18:45:30 -0600

    Update feature completed

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,10 +1,34 @@
 class RolesController < ApplicationController
+  before_action :set_role, only: %i[show edit update destroy]
   def index;
     @roles = Role.all
   end
   def create
     Role.create description: params[:description]
     redirect_to action: :index
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @role.update(branch_params)
+        format.html { redirect_to roles_path, notice: 'Role was successfully updated.' }
+        format.json { render :show, status: :ok, location: @role }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @role.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_role
+    @role = Role.find(params[:id])
   end
 
 end
