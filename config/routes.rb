5eee8199e92af3ce489b8ff372cb7face3013f3a
Javi1499 <fruiz1@ucol.mx>
2022-04-07 17:39:15 -0500

    Attendance by month report fixed
    Co-authored-by: Elías U. Rodríguez <EliasUR@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'admins/login'
   devise_for :admins
   resources :employees
-  resources :branches
+  resources :branches do
+    member do
+      get :filter
+    end
+  end
   resources :records
   resources :roles
   get '/employees/:id/change_status', to: "employees#update_status", as: "status_employee"
-  patch '/branches/filter', to: "branches#attendance_branch", as: "filter"
   root 'records#new'
 end
